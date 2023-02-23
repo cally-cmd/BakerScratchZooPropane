24c34010ea8fcf2d1f5e2efc1f9d51b428a8180e
Ethan Thomas <thomasem@hendrix.edu>
2023-02-22 22:45:24 -0600

    Brought in Cabinet script for changing scenes, edited gameManager

--- Platformer/Assets/Scripts/GameManager.cs
+++ Platformer/Assets/Scripts/GameManager.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {get; private set;}
+
+    //public GameObject dialogBox;
+    //public TextMeshProUGUI dialogText;
+
+    //public GameObject curtain;
+    private bool raiseLower = false;
+
+
+    public GameObject canvas;
+    public GameObject eventSystem;
+
+    public GameObject mainScreen;
     public int collection;
     // Start is called before the first frame update
     void Start()
@@ -26,5 +38,65 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
 
+    }
+
+    IEnumerator ColorLerpFunction(bool fadeout, float duration)
+    {
+        float time = 0;
+        raiseLower = true;
+        //Image curtainImg = curtain.GetComponent<Image>();
+        Color startValue;
+        Color endValue;
+        if (fadeout) {
+            startValue = new Color(0, 0, 0, 0);
+            endValue = new Color(0, 0, 0, 1);
+        } else {
+            startValue = new Color(0, 0, 0, 1);
+            endValue = new Color(0, 0, 0, 0);
+        }
+
+        while (time < duration)
+        {
+            //curtainImg.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        //curtainImg.color = endValue;
+        raiseLower = false;
+    }
+
+    IEnumerator LoadYourAsyncScene(string scene)
+     {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        StartCoroutine(ColorLerpFunction(true, 1));
+
+        while (raiseLower)
+        {
+            yield return null;
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(ColorLerpFunction(false, 1));
+    }
+
+
+    public void EnterCabinet(string scene) {
+        StartCoroutine(LoadYourAsyncScene(scene));
+    }
+
+    public void StartGame() {
+        StartCoroutine(LoadYourAsyncScene("Main"));
+        //mainScreen.SetActive(false);
     }
 }
